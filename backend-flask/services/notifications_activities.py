50fcc31481d17f6d02544dfd1ba59de8c6f20b85
Abdulquadri Sulaimon <sulaimonqlove@gmail.com>
2024-12-19 10:10:53 +0000

    changed notifications

--- backend-flask/services/notifications_activities.py
+++ backend-flask/services/notifications_activities.py
@@ -8,19 +8,37 @@ class NotificationsActivities:
       'message': 'Starlight is fun!',
       'created_at': (now - timedelta(days=2)).isoformat(),
       'expires_at': (now + timedelta(days=5)).isoformat(),
-      'likes_count': 5,
-      'replies_count': 1,
-      'reposts_count': 0,
+      'likes_count': 114,
+      'replies_count': 2,
+      'reposts_count': 67,
       'replies': [{
         'uuid': '26e12864-1c26-5c3a-9658-97a10f8fea67',
         'reply_to_activity_uuid': '68f126b0-1ceb-4a33-88be-d90fa7109eee',
         'handle':  'Worf',
         'message': 'This post has no honor!',
-        'likes_count': 0,
-        'replies_count': 0,
-        'reposts_count': 0,
+        'likes_count': 3,
+        'replies_count': 2,
+        'reposts_count': 6,
         'created_at': (now - timedelta(days=2)).isoformat()
-      }],
+      }, {
+        'uuid': '248959df-3079-4947-b847-9e0892d1bab4',
+        'reply_to_activity_uuid': '68f126b0-1ceb-4a33-88be-d90fa7109eee',
+        'handle':  'Garek',
+        'message': 'This post has more honor!',
+        'likes_count': 4,
+        'replies_count': 5,
+        'reposts_count': 6,
+        'created_at': (now - timedelta(days=2)).isoformat()
+      }]
+    }, 
+    {
+      'uuid': '66e12864-8c26-4c3a-9658-95a10f8fea67',
+      'handle':  'Worf',
+      'message': 'I am out of prune juice',
+      'created_at': (now - timedelta(days=7)).isoformat(),
+      'expires_at': (now + timedelta(days=9)).isoformat(),
+      'likes': 0,
+      'replies': []
     }
     ]
     return results
